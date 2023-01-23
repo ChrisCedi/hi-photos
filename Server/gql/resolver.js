cfbb31cfd44f2ed4c3a1ea80b69a7a7d6da3a81b
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-01-23 17:50:40 -0600

    feature: refactor in resolvers and auto start server with nodemon

--- Server/gql/resolver.js
+++ Server/gql/resolver.js
@@ -1,42 +1,14 @@
-const User = require("../models/user");
-const bcriptjs = require("bcryptjs");
+const userController = require("../controllers/user");
+
 const resolver = {
   Query: {
     //User
-    getUser: () => {
-      console.log("getting user");
-      return null;
-    },
+    getUser: () => userController.getUser,
   },
 
   Mutation: {
     //User
-    register: async (_, { values }) => {
-      const newUser = values;
-      newUser.username = newUser.username.toLowerCase();
-      newUser.email = newUser.email.toLowerCase();
-
-      console.log(newUser);
-
-      const { email, password, username } = newUser;
-
-      const foundEmail = await User.findOne({ email });
-      if (foundEmail) throw new Error("El correo ya esta registrado");
-
-      const foundUsername = await User.findOne({ username });
-      if (foundUsername) throw new Error("El nombre de usuario ya esta en uso");
-
-      const salt = await bcriptjs.genSaltSync(10);
-      newUser.password = await bcriptjs.hash(password, salt);
-
-      try {
-        const user = new User(newUser);
-        user.save();
-        return user;
-      } catch (error) {
-        console.log(error);
-      }
-    },
+    register: async (_, { values }) => userController.register(values),
   },
 };
 
