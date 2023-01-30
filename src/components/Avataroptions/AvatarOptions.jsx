72e2be39ce994571a65630e7b6a69f15170b2c09
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-01-30 17:25:40 -0600

    feature: avatar input to select phoo

--- src/components/Avataroptions/AvatarOptions.jsx
+++ src/components/Avataroptions/AvatarOptions.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Button } from "@material-ui/core";
 import { useStyles } from "./AvatarOptionsStyles";
+import { useDropzone } from "react-dropzone";
 
 export const AvatarOptions = (handleOpen) => {
   const classes = useStyles();
 
+  const onDrop = useCallback((acceptedFile) => {
+    console.log(acceptedFile);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/png",
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
+
   return (
     <div className={classes.container}>
-      <Button color="secondary">Cambiar foto de perfil</Button>
+      <Button color="secondary" {...getRootProps()}>
+        Cargar foto
+      </Button>
       <Button className={classes.delete}>Eliminar foto actual</Button>
 
       <Button onClick={handleOpen}>Cancelar</Button>
+      <input {...getInputProps()} />
     </div>
   );
 };
