d1bdb8dbbc11c11497789c1f85e650856a3f2f2c
ChrisCedi <christian.cedillo.palacios@outlook.com>
2023-01-30 12:46:02 -0600

    feature: profile and user not found views

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_USER } from "../../gql/user";
 import { useStyles } from "./ProfileStyles";
-import { Typography } from "@material-ui/core";
+import { Typography, Avatar, Grid } from "@material-ui/core";
+import { UserNotFound } from "../UserNotFound";
 import { get } from "lodash";
 
 export const Profile = ({ username }) => {
@@ -16,10 +17,29 @@ export const Profile = ({ username }) => {
 
   const dataUser = get(data, "getUser", {});
 
+  console.log(dataUser);
+
   if (loading) return null;
-  if (error) return <Typography>Usuario no encontrado</Typography>;
+  if (error) return <UserNotFound />;
 
   console.log(dataUser);
 
-  return <div>Profile</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12} className={classes.gridAvatar}>
+        <Avatar className={classes.avatar} />
+        <Typography variant="h4" className={classes.username}>
+          {dataUser.username}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} className={classes.gridInfo}>
+        <Typography>{dataUser.name}</Typography>
+        <Typography>{dataUser.description}</Typography>
+        <Typography>{dataUser.email}</Typography>
+      </Grid>
+      <Grid item xs={12} sm={6} className={classes.gridInfo}>
+        <Typography>Followers: 5543</Typography>
+      </Grid>
+    </Grid>
+  );
 };
--- src/views/Profile/ProfileStyles.js
+++ src/views/Profile/ProfileStyles.js
@@ -1,5 +1,36 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  gridAvatar: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    paddingTop: theme.spacing(2),
+  },
+  gridInfo: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+
+    [theme.breakpoints.down("xs")]: {
+      justifyContent: "flex-start",
+    },
+  },
+  avatar: {
+    width: 200,
+    height: 200,
+    border: `5px solid ${theme.palette.secondary.main}`,
+    [theme.breakpoints.down("xs")]: {
+      width: 100,
+      height: 100,
+    },
+  },
+  username: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+}));
 
 export { useStyles };
